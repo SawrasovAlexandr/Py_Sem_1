dd865bc27128537b6b2b0a424141b7cc817cb723
AlexandrSawrasov <caiiiuk@mail.ru>
2022-12-14 23:57:17 +0300

    add task_6

--- Task_2.py
+++ Task_2.py
@@ -8,5 +8,5 @@ number = int(input('Введите число: '))
 sum = 0
 while number != 0:
     sum += number%10
-    number = number//10
-print(f'Сумма цифр введенного числа ровна: {sum}')
+    number //= 10
+print(f'Сумма цифр введенного числа равна: {sum}')
